4d24271f7b4e93124455e594b2965b27282a9fc0
Timothy C. Lam <geroginalam@hotmail.com>
2017-11-18 22:31:41 +0800

    test login

--- api/portal/get.php
+++ api/portal/get.php
@@ -23,6 +23,10 @@
 	if (isset($_GET['student_id']))
 	{
 		//	Retrieve user MMU IDM password to login to MMU Portal
+		
+		//	Login Credentials for MMU Portal
+		$studentID = $_GET['student_id'];
+		$password = "Tasem0707@";
 	}
 	else
 	{
@@ -37,14 +41,18 @@
 	//	URL of MMU Portal
 	$url = "https://online.mmu.edu.my";
 	
+	//	Data for Login POST
+	$data = array('form_loginUsername' => $studentID, 'login-password' => $password);
+	
 	//	Connect to MMU PORTAL with cURL
 	$curl = curl_init($url);
-	curl_setopt($curl, CURLOPT_POST, FALSE);
+	curl_setopt($curl, CURLOPT_POST, TRUE);
 	curl_setopt($curl, CURLOPT_FOLLOWLOCATION, TRUE);
 	curl_setopt($curl, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.93 Safari/537.36");
 	curl_setopt($curl, CURLOPT_HEADER, FALSE);
 	curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
 	curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, FALSE);
+	curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($data));
 	$result = curl_exec($curl);
 	
 	//	Process the return value
