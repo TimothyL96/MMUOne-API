cc0c2146109afbf2e000fe8da4e054484761dec3
Timothy C. Lam <geroginalam@hotmail.com>
2017-11-18 23:11:11 +0800

    test login succeeded

--- api/portal/get.php
+++ api/portal/get.php
@@ -15,16 +15,22 @@
 	//	Connection
 	require_once '../config/connection.php';
 	
+	//	Users object
+	require_once '../objects/users.php';
+	
 	//	Instantiate users object and retrieve connection
 	$db = new Database();
 	$conn = $db->connect();
 	
-	//	Set student ID of user to be read
+	//	Set connection for users table
+	$users = new Users($conn);
+	
+	//	Check if Student ID provided
 	if (isset($_GET['student_id']))
 	{
 		//	Retrieve user MMU IDM password to login to MMU Portal
 		
-		//	Login Credentials for MMU Portal
+		//	Set Login Credentials for MMU Portal
 		$studentID = $_GET['student_id'];
 		$password = "Tasem0707@";
 	}
@@ -55,22 +61,12 @@
 	curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($data));
 	$result = curl_exec($curl);
 	
-	curl_setopt($curl, CURLOPT_POST, TRUE);
-	curl_setopt($curl, CURLOPT_FOLLOWLOCATION, TRUE);
-	curl_setopt($curl, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.93 Safari/537.36");
-	curl_setopt($curl, CURLOPT_HEADER, FALSE);
-	curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
-	curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, FALSE);
-	curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($data));
-	$result = curl_exec($curl);
-	
-	//	cURL error
-	echo 'Error: ' . curl_error($curl) . '\n';
+	//	Get any cURL error
+	curl_error($curl);
 	$status = curl_getinfo($curl, CURLINFO_COOKIELIST); 
-	echo 'HTTP CODE: ' . $status;
 	
 	//	Process the return value
-	echo $result;
+	$result;
 	
 	//	Close cUrl resource and free up system resources
 	curl_close($curl);
