e8c15bd402a47103fabdedd7b77731fdc5270330
Timothy C. Lam <geroginalam@hotmail.com>
2017-11-21 19:05:48 +0800

    test mmls login

---
+++ api/mmls/get.php
@@ -0,0 +1,76 @@
+<?php
+	//	***********************************
+	//
+	//				MMU PORTAL
+	//
+	//	***********************************
+	
+	//	Headers
+	header("Access-Control-Allow-Origin: *");
+	header("Access-Control-Allow-Headers: access");
+	header("Access-Control-Allow-Methods: GET");
+	header("Access-Control-Allow-Credentials: true");
+	header("Content-Type: application/json; charset=UTF-8");
+	
+	//	Connection
+	require_once '../config/connection.php';
+	
+	//	Users object
+	require_once '../objects/users.php';
+	
+	//	Instantiate users object and retrieve connection
+	$db = new Database();
+	$conn = $db->connect();
+	
+	//	Set connection for users table
+	$users = new Users($conn);
+	
+	//	Check if Student ID provided
+	if (!empty($_GET['student_id']))
+	{
+		//	Retrieve user MMU IDM password to login to MMU Portal
+		
+		//	Set Login Credentials for MMU Portal
+		$studentID = $_GET['student_id'];
+		$password = "Tasem0707@";
+	}
+	else
+	{
+		//	Set error
+		
+		//	Echo JSON message
+		
+		//	Kill
+		die("No student ID specified");
+	};
+	
+	//	URL of MMU Portal
+	$url = "https://mmls.mmu.edu.my/checklogin";
+	
+	//	Data for Login POST
+	$data = array('stud_id' => $studentID, 'stud_pswrd' => $password);
+	
+	//	Create cookie file
+	$cookie = tempnam("/cookie", "CURLCOOKIE");
+	
+	//	Connect to MMU PORTAL with cURL
+	$curl = curl_init($url);
+	curl_setopt($curl, CURLOPT_POST, TRUE);
+	curl_setopt($curl, CURLOPT_FOLLOWLOCATION, TRUE);
+	curl_setopt($curl, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.93 Safari/537.36");
+	curl_setopt($curl, CURLOPT_HEADER, FALSE);
+	curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
+	curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, FALSE);
+	curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($data));
+	curl_setopt($curl, CURLOPT_COOKIEJAR, $cookie);
+	$result = curl_exec($curl);
+	
+	//	Get any cURL error
+	curl_error($curl);
+	$status = curl_getinfo($curl, CURLINFO_COOKIELIST); 
+	
+	//	Process the return value
+	echo $result;
+	
+	//	Close cUrl resource and free up system resources
+	curl_close($curl);
