0d4f807aed5757ed3bc901dd012aaee428926fa7
Timothy C. Lam <geroginalam@hotmail.com>
2017-11-24 21:47:15 +0800

    test portal news

--- api/portal/get.php
+++ api/portal/get.php
@@ -106,7 +106,7 @@
 	$inputContent = $htmlDOM->find('div[id=tabs-1] div.bulletinContentAll');
 	
 	//	Print the result
-	print_r($inputContent[0]->plaintext);
+	echo htmlspecialchars_decode($inputContent[0]->plaintext);
 	
 	//	Close cUrl resource and free up system resources
 	curl_close($curl);
