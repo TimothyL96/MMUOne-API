a7b1a7272620010f4708a8ad72a41b24e9ff7e75
ttimt <geroginalam@hotmail.com>
2018-06-09 02:09:35 +0800

    added token class

--- api/objects/token.php
+++ api/objects/token.php
@@ -12,6 +12,9 @@
 		private $tableName = "token";
 
 		//	Public members
+		public $student_id;
+		public $macAddr;
+		public $token;
 
 		/**
 		 * Portal constructor. Get database connection and put in the private class member for connection
@@ -32,4 +35,114 @@
 
 		}
 
+		function insertNewUser()
+		{
+			$query = "SELECT * FROM {$this->tableName} WHERE student_id = ?";
+
+			$stmt = $this->conn->prepare($query);
+
+			$stmt->bindParams(1, $this->student_id);
+
+			if (!$stmt->execute())
+			{
+				return FALSE;
+			}
+
+			$stmt->store_result();
+
+			if ($stmt->num_rows == 1)
+			{
+				return TRUE;
+			}
+
+			//	Row does not exist, create new row
+			$query = "INSERT INTO {$this->tableName} (student_id) VALUES (?)";
+
+			$stmt = $this->conn->prepare($query);
+
+			$stmt->bindParams($this->student_id);
+
+			if (!$stmt->execute())
+			{
+				return FALSE;
+			}
+
+			return TRUE;
+		}
+
+		function getMacAddr()
+		{
+			$query = "SELECT mac_addr FROM {$this->tableName} WHERE student_id = ?";
+
+			$stmt = $this->conn->prepare($query);
+
+			$stmt->bindParams($query);
+
+			if (!$stmt->execute())
+			{
+				return FALSE;
+			}
+
+			$row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+			$this->macAddr = $row['mac_addr'];
+
+			return TRUE;
+		}
+
+		function getToken()
+		{
+			$query = "SELECT token FROM {$this->tableName} WHERE student_id = ?";
+
+			$stmt = $this->conn->prepare($query);
+
+			$stmt->bindParams(1, $this->student_id);
+
+			if (!$stmt->execute())
+			{
+				return FALSE;
+			}
+
+			$row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+			$this->token = $row['token'];
+
+			return TRUE;
+		}
+
+		function updateMacAddr($macAddr)
+		{
+			$query = "UPDATE {$this->tableName} SET mac_addr = ? WHERE student_id = ?";
+
+			$stmt = $this->conn->prepare($query);
+
+			$stmt->bindParams(1, $macAddr);
+			$stmt->bindParams(2, $this->student_id);
+
+			if (!$stmt->execute())
+			{
+				return FALSE;
+			}
+
+			return TRUE;
+		}
+
+		//	Update token and date time
+		function updateTable($token, $dateTime)
+		{
+			$query = "UPDATE {$this->tableName} SET token = :token, datetime = :dateTime WHERE student_id = :studentID";
+
+			$stmt = $this->conn->prepare($query);
+
+			$stmt->bindParams(":token", $token);
+			$stmt->bindParams(":dateTime", $dateTime);
+			$stmt->bindParams(":studentID", $this->student_id);
+
+			if (!$stmt->execute())
+			{
+				return FALSE;
+			}
+
+			return TRUE;
+		}
 	}
--- api/objects/tokenValidation.php
+++ api/objects/tokenValidation.php
@@ -6,6 +6,12 @@
 	 * Time: 12:55 AM
 	 */
 
+	//	Require the token class
+	require_once 'token.php';
+
+	//	Require the connection class
+	require_once '../config/connection.php';
+
 	//	Check whether input token is valid or not
 	function tokenValidation()
 	{
